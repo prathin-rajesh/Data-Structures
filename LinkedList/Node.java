af8bc0ae3ce5cf718474f76d228abad070442a38
Prathin <59028729+prathin-rajesh@users.noreply.github.com>
2024-08-20 18:37:27 +0530

    Update Node.java

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -1,1 +1,21 @@
+public class Node {
+    int data;
+    Node next; //by default next is null
+    
+    //no arg constructor
+    public Node (){
 
+    }
+    
+    //parameterized constructor
+    public Node(int data){
+        this.data=data;
+    }
+
+    //parameterized constructor when two arguments are passed
+    // used particularly when we need to add node in the middle of linked list
+    public Node(int data, Node next){
+        this.data=data;
+        this.next=next; 
+    }
+}
